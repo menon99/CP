9b285d0c0b8cc2dcdea92986617a0b070389819c
akash <akashmenon11699cool@hotmail.com>
2020-04-14 23:10:59 +0530

    truly nlogn now lol

--- arrays/longest_increasing_subsequence_patience_sort.cpp
+++ arrays/longest_increasing_subsequence_patience_sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <fstream>
 
 using namespace std;
 
@@ -13,45 +14,58 @@ using namespace std;
 #define getarr(arr, n) f(0, i, i < n, 1) cin >> arr[i];
 #define eb emplace_back
 
+int bsearch(int k, vector<int> arr,vector<vector<int>> lis)
+{
+    int ll = 0, ans = -1, rl = lis.size(), mid;
+    while (ll <= rl && ll < lis.size())
+    {
+        mid = (ll + rl) / 2;
+        if (arr[lis[mid][lis[mid].size() -1]]< k)
+            ll = mid + 1;
+        else
+        {
+            ans = mid;
+            rl = mid - 1;
+        }
+    }
+    return ans;
+}
+
 int main(int argc, char const *argv[])
 {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    int n, ele, link;
+    int n, ele, index;
     cin >> n;
     vector<int> arr(n);
     getarr(arr, n);
     vector<vector<int>> lis;
     unordered_map<int, int> links;
-    bool flag;
     f(0, i, i < n, 1)
     {
-        flag = false;
-        link = -1;
-        f(0, j, j < lis.size(), 1)
-        {
-            ele = lis[j][lis[j].size() - 1];
-            if (arr[ele] > arr[i])
-            {
-                lis[j].eb(i);
-                links[i] = link;
-                flag = true;
-                break;
-            }
-            else
-                link = ele;
-        }
-        if (!flag)
+        index = bsearch(arr[i],arr,lis);
+        if(index == -1)
         {
             vector<int> a;
+            if(lis.size() == 0)
+                links[i] = -1;
+            else
+                links[i] = lis[lis.size() -1][lis[lis.size() - 1].size() -1];
             a.eb(i);
             lis.eb(a);
-            links[i] = link;
+        }
+        else
+        {
+            lis[index].eb(i);
+            if(index == 0)
+                links[i] = -1;
+            else
+                links[i] = lis[index-1][lis[index-1].size() -1];
         }
     }
-    ele = lis[lis.size() -1][lis[lis.size()-1].size() -1];
+    ele = lis[lis.size() - 1][lis[lis.size() - 1].size() - 1];
     log2("longest incresing subsequence is :");
-    while(ele != -1)
+    while (ele != -1)
     {
         log3(arr[ele]);
         ele = links[ele];
