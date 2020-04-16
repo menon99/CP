c2fba660de3498fe73a0bf6a7258f320f55999b7
akash <akashmenon11699cool@hotmail.com>
2020-04-16 14:56:40 +0530

    test4

--- playground/hello.cpp
+++ playground/hello.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-<<<<<<< HEAD
     cout << "hello";
-=======
-    cout << "hello  akash";
->>>>>>> 3fe834a548e28510c6cd0b35d2dbe5149b43dd2d
     return 0;
 }
