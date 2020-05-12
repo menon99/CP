cc2cfba1d4731c7d10e5aef47f1e823d4087767f
Akash <akashmenon11699cool@hotmail.com>
2020-05-12 13:55:00 +0530

    height of  a tree

---
+++ trees/height.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "tree.h"
+
+using namespace std;
+
+int height(node *root, int h)
+{
+    if (root == NULL)
+        return h;
+    return max(height(root->left, h + 1), height(root->right, h + 1));
+}
+
+int main(int argc, char const *argv[])
+{
+    tree t1;
+    int n,data;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >>data;
+        t1.insert(data);
+    }
+    cout << "height of tree is :" << height(t1.root,0) << endl;
+    return 0;
+}
