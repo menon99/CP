853926a41841632b5d0aed0c56f4e086f8aa6f6e
Akash <akashmenon11699cool@hotmail.com>
2020-05-26 15:09:41 +0530

    extra functions

--- graph/graph.h
+++ graph/graph.h
@@ -13,21 +13,48 @@ private:
 
 public:
     vector<unordered_map<int, int>> g1;
+    //constructor number of nodes in graph
     graph(int n)
     {
         nodes = n;
         g1.resize(n + 1);
     }
+
+    //returns no. of nodes in graph
+    int getNumNodes()
+    {
+        return nodes;
+    }
+
+    //returns edges of a node
+    unordered_map<int,int> getEdges(int node)
+    {
+        return g1[node];
+    }
+
+    //returns distance between two nodes if exists
+    int getDist(int node1,int node2)
+    {
+        if(g1[node1].find(node2) != g1[node1].end())
+            return g1[node1][node2];
+        return INT32_MAX;
+    }
+
+    //inserts all edges from a given vertex
     bool insert(int index, unordered_map<int, int> m1)
     {
         g1[index] = m1;
         return true;
     }
-    bool addEdge(int index, int edge, int cost = 0)
+
+    //edge from v1 to v2 with cost 
+    bool addEdge(int v1, int v2, int cost = 0)
     {
-        g1[index][edge] = cost;
+        g1[v1][v2] = cost;
         return true;
     }
+
+    //breadth first search traversal
     void bfs(int start)
     {
         queue<pair<int, int>> q1;
@@ -49,6 +76,8 @@ public:
             }
         }
     }
+
+    //depth first search traversal
     void dfs(int start)
     {
         vector<bool> visited(nodes + 1, false);
